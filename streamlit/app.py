3914da9ab2e8f164f6646c7c7d7575d8832b20bf
Kabilan N <60337704+Kabilan-n@users.noreply.github.com>
2022-01-30 01:20:39 +0530

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -12,9 +12,8 @@ import cv2
 import streamlit as st
 
 uploaded_file = st.file_uploader("Choose a file")
-
-    if uploaded_file is not None:
-        # To read file as bytes:
-        bytes_data = uploaded_file.read()
-        st.write(bytes_data)
-        print(bytes_data)
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.read()
+    st.write(bytes_data)
+    print(bytes_data)
