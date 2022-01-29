f55ec635356975a82d41ed6a8213eb517702e021
Kabilan N <60337704+Kabilan-n@users.noreply.github.com>
2022-01-30 01:23:09 +0530

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -15,5 +15,5 @@ uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
     # To read file as bytes:
     bytes_data = uploaded_file.read()
-    st.write(bytes_data)
+    st.write(hello)
     print(bytes_data)
