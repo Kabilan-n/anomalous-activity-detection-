815277192fb7738c5293d37d3fa9d16f8b26dd20
Kabilan N <60337704+Kabilan-n@users.noreply.github.com>
2022-01-30 09:16:00 +0530

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -12,9 +12,32 @@ import cv2
 import streamlit as st
 
 uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-    video_file = open(uploaded_file, 'rb')
-    video_bytes = video_file.read()
+# if uploaded_file is not None:
+#     video_file = open(uploaded_file, 'rb')
+#     video_bytes = video_file.read()
+    
+import streamlit as st
+import cv2 as cv
+import tempfile
+
+f = st.file_uploader("Upload file")
+
+tfile = tempfile.NamedTemporaryFile(delete=False) 
+tfile.write(uploaded_file.read())
+
+
+vf = cv.VideoCapture(tfile.name)
+
+stframe = st.empty()
+
+while vf.isOpened():
+    ret, frame = vf.read()
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    stframe.image(gray)
 
 # st.video(video_bytes)
 #     # To read file as bytes:
