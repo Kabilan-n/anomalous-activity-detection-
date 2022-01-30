8e348b33fb4c32628e331ee0498561973b4d6949
Kabilan N <60337704+Kabilan-n@users.noreply.github.com>
2022-01-30 09:39:06 +0530

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -16,21 +16,36 @@ import tempfile
 f = st.file_uploader("Upload file")
 
 tfile = tempfile.NamedTemporaryFile(delete=False) 
-tfile.write(f.read())
+# tfile.write(f.read())
+import streamlit as st
+import time
+import cv2
 
+if st.button('Start'):
+    video = cv2.VideoCapture('https://www.mediacollege.com/video-gallery/testclips/20051210-w50s.flv')
+    video.set(cv2.CAP_PROP_FPS, 25)
+
+    image_placeholder = st.empty()
+
+    while True:
+        success, image = video.read()
+        if not success:
+            break
+        image_placeholder.image(image, channels="BGR")
+        time.sleep(0.01)
 
 vf = cv.VideoCapture(tfile.name)
 
-stframe = st.empty()
-outframe = []
-while vf.isOpened():
-    ret, frame = vf.read()
-    # if frame is read correctly ret is True
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    outframe.append(gray)
-st.video(outframe)
+# stframe = st.empty()
+# outframe = []
+# while vf.isOpened():
+#     ret, frame = vf.read()
+#     # if frame is read correctly ret is True
+#     if not ret:
+#         print("Can't receive frame (stream end?). Exiting ...")
+#         break
+#     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+#     outframe.append(gray)
+# st.video(outframe)
     #stframe.image(gray)
     
